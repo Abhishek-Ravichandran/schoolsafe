4869c280e9c4bba20eebd2cd484ce4fcab156bc1
Donna <dcstlouis@gmail.com>
2016-11-13 16:17:08 +0000

    Added links to school pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,20 @@
+# == Route Map
+#
+#  Prefix Verb URI Pattern            Controller#Action
+#    root GET  /                      static_pages#home
+#         POST /                      static_pages#home
+#   about GET  /about(.:format)       static_pages#about
+# contact GET  /contact(.:format)     static_pages#contact
+# schools GET  /schools(.:format)     schools#index
+#  school GET  /schools/:id(.:format) schools#show
+#
+
 Rails.application.routes.draw do
   root 'static_pages#home'
   post '/', to: 'static_pages#home'
   get 'about', to: 'static_pages#about'
   get 'contact', to: 'static_pages#contact'
+  
+  resources :schools, only: [:index, :show]
   
 end
